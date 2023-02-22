243249822c89d248f7fa914c63694462233f2521
minki3 <86992006+minki3@users.noreply.github.com>
2023-02-22 19:39:04 +0900

    react-query

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import Redux from "./pages/reudx/Redux";
+import ReactQuery from "./pages/reactQuery/ReactQuery";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element=<MainPage /> />
         <Route path="/redux" element=<Redux /> />
+        <Route path="/reactquery" element=<ReactQuery /> />
       </Routes>
     </BrowserRouter>
   );
