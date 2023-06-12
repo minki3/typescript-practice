0f95572ca612830158f53e0b4c2551cc016cc07d
minki3 <86992006+minki3@users.noreply.github.com>
2023-06-12 22:43:09 +0900

    Feat:tailwindcss적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import Redux from "./pages/reudx/Redux";
 import RtkQuery from "./pages/rtkQuery/rtkQuery";
 import Page from "./pages/responsiveWeb/Page";
 import ReactQuery from "./pages/react-query/ReactQuery";
+import Practice from "./pages/react-hook-form/Practice";
 
 function Router() {
   return (
@@ -15,6 +16,7 @@ function Router() {
         <Route path="/rtkquery" element=<RtkQuery /> />
         <Route path="/page" element=<Page /> />
         <Route path="/reactquery" element=<ReactQuery /> />
+        <Route path="/reactform" element=<Practice /> />
       </Routes>
     </BrowserRouter>
   );
