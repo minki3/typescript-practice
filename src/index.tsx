3ab917632d49f74ea8f8e24bb2ed1dcb608c6a52
minki3 <86992006+minki3@users.noreply.github.com>
2023-06-16 20:33:16 +0900

    Feat:usefieldarray

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,16 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
+import ReactPDF from "@react-pdf/renderer";
+import FormPage from "./pages/react-hook-form/Form";
+import ReactPDFView from "@react-pdf/renderer";
+
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
+
+// ReactPDF.render(<FormPage />, `${__dirname}/example.pdf`);
+// ReactPDFView.renderToStream(<FormPage />);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/pages/react-hook-form/Practice.tsx
+++ src/pages/react-hook-form/Practice.tsx
@@ -2,19 +2,67 @@ import React, { useState } from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 import Form from "./Form";
 import View from "./View";
+import { PDFViewer } from "@react-pdf/renderer";
+import ReactPDF from "@react-pdf/renderer";
+import jsPDF from "jspdf";
 
+interface InitalType {
+  name: string[];
+}
+
+const initalValue: InitalType = {
+  name: [],
+};
 export default function Practice() {
-  const { control, register } = useForm();
+  const { control, handleSubmit } = useForm({
+    mode: "onBlur",
+    // defaultValues: initalValue,
+  });
   const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
     {
       control, // control props comes from useForm (optional: if you are using FormContext)
-      name: "test", // unique name for your Field Array
+      name: "name", // unique name for your Field Array
     }
   );
 
+  const submit = (data: any) => {
+    console.log(data);
+  };
+  const handlePrintToPDF = () => {
+    window.print();
+  };
+  // const handlePrintToPDF = () => {
+  //   const doc = new jsPDF();
+  //   const element = document.getElementById("pdf-content");
+
+  //   doc.html(element, {
+  //     callback: function (pdf) {
+  //       pdf.save("document.pdf");
+  //     },
+  //     x: 10,
+  //     y: 10,
+  //   });
+  // };
+
   return (
-    <div className="flex">
-      <input />
+    <div id="pdf-content" className="flex">
+      <form onSubmit={handleSubmit(submit)}>
+        {fields.map((item, idx) => {
+          return (
+            <div key={idx}>
+              <input />
+              <p>sdafsf</p>
+            </div>
+          );
+        })}
+      </form>
+      <p>sdaffas</p>
+      <PDFViewer>
+        <Form />
+      </PDFViewer>
+      <button onClick={handlePrintToPDF}>Save as PDF</button>
     </div>
   );
 }
+// ReactPDF.render(<Form />, `${__dirname}/example.pdf`);
+// ReactDOM.render(<App />, document.getElementById("root"));
